6dbcbb7c4c8faa6828c433f4d0035f21ec881b72
dkckjcjf <sodka20071@gmail.com>
2025-11-19 17:04:34 +0300

    Update main (1).py

--- main (1).py
+++ main (1).py
@@ -1,15 +1,1 @@
 
-import telebot
-
-# Укажите токен вашего бота, полученный от BotFather
-TOKEN = '8415064614:AAHZeLTseqkYn5bLXV0XAkScYIxe7IlqUeY'
-
-bot = telebot.TeleBot(TOKEN)
-
-@bot.message_handler(func=lambda message: True)
-def echo_message(message):
-    bot.reply_to(message, message.text)
-
-if __name__ == "__main__":
-    print("Запускаем бота...")
-    bot.polling()
